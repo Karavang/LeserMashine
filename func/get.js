8e1a70d4a97e25ee49a144882d83f1e48ee4671e
Karavang <k4r4v4ng@gmail.com>
2024-07-14 23:26:54 +0200

    14.07

--- func/get.js
+++ func/get.js
@@ -1,21 +1,17 @@
-const AWS = require("aws-sdk");
-const s3 = new AWS.S3();
-const getAllBooks = async () => {
-  await s3.listObjectsV2(
-    {
-      Bucket: "elasticbeanstalk-eu-west-3-507450525930",
-      Prefix: "books/",
-    },
-    function (err, data) {
-      if (err) {
-        console.log("Error", err);
-      } else {
-        console.log("Success", data.Contents);
-        data.Contents.forEach((object) => {
-          console.log(object);
-        });
-      }
-    },
-  );
+const { GetObjectCommand } = require("@aws-sdk/client-s3");
+
+const getAllBooks = async (req, res) => {
+  const input = {
+    Bucket: "elasticbeanstalk-eu-west-3-507450525930",
+    Key: "test.txt",
+  };
+  const command = new GetObjectCommand(input);
+  const response = await client.send(command);
+
+  if (!response) {
+    res.status(500).json(response);
+  } else {
+    res.status(200).json(response);
+  }
 };
 module.exports = getAllBooks;
