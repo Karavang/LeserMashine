62167e9a85d96f636c9af25035da7ae5ef44cb14
Karavang <k4r4v4ng@gmail.com>
2024-10-14 16:49:14 +0200

    s3 v2 -> v3

--- func/get.js
+++ func/get.js
@@ -1,9 +1,10 @@
-const s3 = require("../import3");
+const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
-const { GetObjectCommand } = require("@aws-sdk/client-s3");
 const app = express();
+
+const s3Client = new S3Client({ region: "eu-west-3" }); // Replace with your region
 
 // Ensure the timeBooks directory exists
 
@@ -13,14 +14,13 @@ const getBookContent = async (req, res) => {
   const booksDir = path.join(__dirname, "../timeBooks/books");
   try {
     const name = req.params.filename;
-    const data = await s3.send(
-      new GetObjectCommand({
-        Bucket: "elasticbeanstalk-eu-west-3-507450525930/books",
-        Key: name,
-      }),
-    );
+    const command = new GetObjectCommand({
+      Bucket: "elasticbeanstalk-eu-west-3-507450525930",
+      Key: `books/${name}`,
+    });
 
-    const epubBuffer = Buffer.from(data.Body);
+    const { Body } = await s3Client.send(command);
+    const epubBuffer = await streamToBuffer(Body);
     const filePath = path.join(booksDir, name);
 
     // Write the file synchronously
@@ -32,7 +32,7 @@ const getBookContent = async (req, res) => {
     // Return the file URL
     res.status(200).send(`http://localhost:5555/books/${name}`);
   } catch (error) {
-    if (error.code === "NoSuchKey") {
+    if (error.name === "NoSuchKey") {
       res.status(404).send("File not found");
     } else {
       console.error("Error fetching the book:", error);
@@ -40,5 +40,13 @@ const getBookContent = async (req, res) => {
     }
   }
 };
+
+async function streamToBuffer(stream) {
+  const chunks = [];
+  for await (const chunk of stream) {
+    chunks.push(chunk);
+  }
+  return Buffer.concat(chunks);
+}
 
 module.exports = getBookContent;
