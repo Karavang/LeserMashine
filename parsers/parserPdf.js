1fd937d0045d603b3e557488f43e5fd0dc9582ce
Karavang <k4r4v4ng@gmail.com>
2024-10-17 15:55:04 +0200

    pdf uploading is done

--- parsers/parserPdf.js
+++ parsers/parserPdf.js
@@ -2,6 +2,13 @@ const pdf = require("pdf-parse");
 
 const parserPdf = async (data, name) => {
   const pdfData = await pdf(data);
-  console.log(pdfData);
+  return {
+    title: pdfData.info.Title,
+    author: pdfData.info.Author,
+    date: pdfData.metadata._metadata["dc:date"].substring(0, 4),
+    lang: pdfData.metadata._metadata["dc:lang"],
+    desc: pdfData.metadata._metadata["dc:description"],
+    filename: name,
+  };
 };
 module.exports = parserPdf;
